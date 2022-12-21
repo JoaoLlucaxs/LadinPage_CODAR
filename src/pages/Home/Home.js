68c62211d8fa561633d9bd3bca6ee55d7a0823d2
devjoao <joaolucasqueiriz@gmail.com>
2022-12-21 10:55:42 -0300

    version BETA

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,7 @@ import Head from '../../Components/Head/Head'
 import Button from '../../Components/Button/Button'
 import Plans from '../../Components/Plans/Plans'
 import {FiSlack} from 'react-icons/fi'
-import Ilustrator from '../../assets/CODAR.png'
+import Ilustrator from '../../assets/ilustrator.png'
 import Input from '../../Components/Input/Input'
 
 const detail=[
@@ -43,9 +43,10 @@ function Home() {
         </Link>
       </div>
     </section>
+
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fb1" fill-opacity="1" d="M0,96L30,106.7C60,117,120,139,180,133.3C240,128,300,96,360,117.3C420,139,480,213,540,229.3C600,245,660,203,720,186.7C780,171,840,181,900,202.7C960,224,1020,256,1080,261.3C1140,267,1200,245,1260,213.3C1320,181,1380,139,1410,117.3L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"></path></svg>
 
-    <section className='details'>
+    <div className='details'>
     <ul>
     {detail.map((item)=>(
       <li>
@@ -56,7 +57,7 @@ function Home() {
       <div className='ilustrator'>
        <img src={Ilustrator} alt='ilustrator ladin-page'/>
       </div>
-  </section>
+  </div>
       <Plans/>
  
     <h2 className='section_Tel'>Digite seu telefone</h2>
